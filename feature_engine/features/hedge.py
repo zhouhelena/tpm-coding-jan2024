0e9fd2d5800dd03ba88bb756187c3fa1774ce0b6
PriyaDCosta <76833529+PriyaDCosta@users.noreply.github.com>
2024-01-11 09:20:42 +0530

    files deleted

--- feature_engine/features/hedge.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import re
-
-#returns the total number of hedge words present in the data set (We can add more hedge words)
-def is_hedged_sentence_1(num_hedge_words):
-    return 1 if num_hedge_words > 0 else 0
